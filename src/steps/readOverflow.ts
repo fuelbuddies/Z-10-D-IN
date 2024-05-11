e0310d2750e3792cbfd248716f09340d5337bb97
Nitin Sharma <nitin@fuelbuddy.in>
2024-05-11 13:09:34 +0400

    NO_T fixing release.
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- src/steps/readOverflow.ts
+++ src/steps/readOverflow.ts
@@ -7,7 +7,7 @@ export class ReadOverflowRegister extends StepBody {
     public overflowCount: number = 0;
 
     public async run(context: StepExecutionContext): Promise<ExecutionResult> {
-        console.log("Read Overflow Register");
+        console.log("Read Overflow Register: ", this.overflowRegister);
         const overflowCounter = await this.client.readHoldingRegisters(this.overflowRegister, 1);
         console.log(overflowCounter);
         this.overflowCount = overflowCounter.buffer.readUint16BE(0);
