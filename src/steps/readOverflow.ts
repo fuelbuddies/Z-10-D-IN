ebf3021fa20f5dffac124e3c4f665f007ddf19fd
Nitin Sharma <nitin@fuelbuddy.in>
2024-05-11 12:19:28 +0400

    NO_T fixing release
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- src/steps/readOverflow.ts
+++ src/steps/readOverflow.ts
@@ -3,13 +3,14 @@ import { ExecutionResult, StepBody, StepExecutionContext } from "workflow-es";
 
 export class ReadOverflowRegister extends StepBody {
     public client: ModbusRTU = new ModbusRTU();
-    public overflowRegister: number = 0;
+    public overflowRegister: number = 8;
     public overflowCount: number = 0;
 
     public async run(context: StepExecutionContext): Promise<ExecutionResult> {
         console.log("Read Overflow Register");
-        // const overflowCounter = await this.client.readHoldingRegisters(this.overflowRegister, 1);
-        // this.overflowCount = overflowCounter.buffer.readUint32BE(0);
+        const overflowCounter = await this.client.readHoldingRegisters(this.overflowRegister, 1);
+        this.overflowCount = overflowCounter.buffer.readUint16BE(0);
+        console.log(`Overflow Count: ${this.overflowCount}`);
         return ExecutionResult.next();
     }
 }
